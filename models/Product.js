3d1f2079f50ad7b4c0dba9e32e78cb832a00f902
nasty-xd <nastyaa.kli5@gmail.com>
2025-12-10 11:05:06 +0200

    Update Product.js

--- models/Product.js
+++ models/Product.js
@@ -7,8 +7,6 @@ module.exports = (sequelize) => {
             id: { type: DataTypes.INTEGER.UNSIGNED, autoIncrement: true, primaryKey: true },
             name: { type: DataTypes.STRING(255), allowNull: false },
             desc: { type: DataTypes.TEXT, allowNull: false },
-
-            // 👉 ДОБАВЛЕНО поле category
             category: { type: DataTypes.STRING(255), allowNull: false, defaultValue: 'Not specified' },
 
             price: { type: DataTypes.DECIMAL(10, 2), allowNull: false, defaultValue: 0 },
